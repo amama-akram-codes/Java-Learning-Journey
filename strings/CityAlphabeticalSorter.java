b65c925e3a55e2fe62228ff105b5340855831674
amama-akram-codes <amama.akram.pk@gmail.com>
2026-08-27 16:46:35 +0500

    Add files via upload

---
+++ strings/CityAlphabeticalSorter.java
@@ -0,0 +1,21 @@
+package com.learnjava.strings;
+import java.util.Scanner;
+public class CityAlphabeticalSorter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		        try (Scanner input = new Scanner(System.in)) {
+					System.out.print("Enter the first city: ");
+					String city1 = input.nextLine();
+					System.out.print("Enter the second city: ");
+					String city2 = input.nextLine();
+					if (city1.compareTo(city2) < 0)
+					    System.out.println("The cities in alphabetical order are " + city1 + " " + city2);
+					else
+					    System.out.println("The cities in alphabetical order are " + city2 + " " + city1);
+				}
+		    }
+	
+}
