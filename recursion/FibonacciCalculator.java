fe86907325f433f9072ed7e1bcfc355977322d4c
amama-akram-codes <amama.akram.pk@gmail.com>
2026-08-31 14:16:41 +0500

    Add files via upload

---
+++ recursion/FibonacciCalculator.java
@@ -0,0 +1,15 @@
+package com.learnjava.recursion;
+
+public class FibonacciCalculator {
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        int a = fib(n - 1);
+        int b = fib(n - 2);
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("Fibonacci(" + n + ") = " + fib(n));
+    }
+}
